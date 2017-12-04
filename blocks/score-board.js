a14c536607cf81e49a9eb104559b5cafbf09db32
Cody Loyd <codyloyd@gmail.com>
2017-12-04 08:47:55 -0600

    add styles

--- blocks/score-board.js
+++ blocks/score-board.js
@@ -1,6 +1,5 @@
 export default function(state, attributes) {
   return div(attributes,
-    h3('Your Score'),
-    h5(`Score: ${state.score}`),
-    h5(`Level: ${state.level}`))
+    div(`Score: ${state.score}`),
+    div(`Level: ${state.level}`))
 };
--- elements/main-controls/main-controls.js
+++ elements/main-controls/main-controls.js
@@ -6,8 +6,14 @@ export class MainControls extends TaDom.TaDomElement {
     // return link({rel:'stylesheet', href:'/elements/main-controls/main-controls.css'});
     return `
     button {
-      background-color: var(--button-bg-color);
-      color: var(--button-text-color);
+      background: var(--blue);
+      margin: 1rem;
+      padding: 1rem 3rem;
+      font-size: 1.5rem;
+      font-family: Quicksand, sans-serif;
+      color: var(--white);
+      box-shadow: var(--box-shadow);
+      cursor: pointer;
     }
     `;
   }
